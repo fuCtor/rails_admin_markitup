c3a1f520e0ec5b48eae4e6a1aa608416c0e6597b
Robert Whitney <robertj.whitney@gmail.com>
2014-06-05 16:59:14 -0400

    adds assets to precompile

--- lib/rails_admin_markitup/engine.rb
+++ lib/rails_admin_markitup/engine.rb
@@ -9,5 +9,14 @@ module RailsAdminMarkitup
     initializer "RailsAdminMarkitup.view_helpers" do
       ActionView::Base.send :include, RailsAdminMarkitup::ViewHelpers
     end
+
+    initializer 'RailsAdminMarkitup precompile hook', group: :all do |app|
+
+      app.config.assets.precompile += %w[
+        markitup.js
+        markitup/sets/markdown/set.js
+        markitup/sets/markdown/style.scss
+      ]
+    end
   end
 end
